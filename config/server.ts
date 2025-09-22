ead8a68c8bca0cdb81b6cdd7e23578011a8e0a91
gravitad-dev <synergiart.websupp@gmail.com>
2025-09-22 08:06:51 +0000

    add docker files

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,11 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  app: { keys: env.array('APP_KEYS') },
+  url: 'https://strapiv2.gravitad.xyz',
+  proxy: true,
+  transfer: {
+    remote: { enabled: true },                 // <- habilita transfer remotas
+    token: { salt: env('TRANSFER_TOKEN_SALT') } // <- usa tu SALT
   },
 });
